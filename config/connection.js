3f359685f7188849df7a8f8b203d79b56fc63dad
L Lee <lauralee.sh@gmail.com>
2018-09-13 14:03:00 -0700

    updated connection with JAWSDB

---
+++ config/connection.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "burger_db"
+// });
+
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+    
+      user: "root",
+    
+      password: "password",
+      database: "burger_DB"
+    });
+};
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// Export connection for ORM to use.
+module.exports = connection;
